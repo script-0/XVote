5593744a42a5e4b9a5b00058ae6ac971f0b3cbb3
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-05-14 09:46:33 +0100

    Signing all commit

--- login_query.php
+++ login_query.php
@@ -4,8 +4,7 @@
 	if(isset($_POST['login'])){
 		$idno=$_POST['idno'];
 		$password=$_POST['password'];
-		$isDisable = true;
-		$result = $conn->query("SELECT voters_id, status FROM voters WHERE id_number = '$idno' && password = '$password'") or die(mysqli_errno());
+		$result = $conn->query("SELECT voters_id, status, account FROM voters WHERE id_number = '$idno' && password = '$password'") or die(mysqli_errno());
 		$numberOfRows = $result->num_rows;
 		if($numberOfRows > 0){
 			$row = $result->fetch_array();
@@ -18,19 +17,26 @@
 						</center>";
 				$isDisable = false;
 			}
-			session_start();
-			$_SESSION['voters_id'] = $row['voters_id'];
-			header('location:vote.php');
-		}
-		
-		if($isDisable){
+			else if( ! $row['account'] == 'Active'){
+				echo " <center>
+						<br>
+						<font color= 'red' size='3'>Your account is still inactive</font>
+						<br>
+						Results Available <a href='results.php?token=CESAGI2022'> here </a>
+				   </center>";
+			}
+			else{
+				session_start();
+				$_SESSION['voters_id'] = $row['voters_id'];
+				header('location:vote.php');
+			}
+		}else{
 			echo " <center>
 						<br>
-						<font color= 'red' size='3'>Your account is still disabled</font>
+						<font color= 'red' size='3'>Bad Matricule / password </font>
 						<br>
 						Results Available <a href='results.php?token=CESAGI2022'> here </a>
 				   </center>";
 		}
-	
 	}
 ?>
