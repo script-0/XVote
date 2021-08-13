4682d3bbbf36a3cb214bf19a27fe504653b957e1
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-08-13 09:31:37 +0100

    adding init_db.sql

--- register/dbcon.php
+++ register/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'voting');
+	$conn = new mysqli('db4free.net', 'cesa_user', 'cesa_password', 'cesa_vote');
 	
 	if(!$conn){
 		die("Error: Failed to connect to database");
