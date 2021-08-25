490bdfa819325ee9850e821157557a021db1b71e
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-08-25 03:59:11 +0100

    Update script.php for deploying on Heroku

--- scripts/script.php
+++ scripts/script.php
@@ -1,4 +1,43 @@
   <!-- jQuery Version 1.11.0 -->
+    <script src="/js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="/js/plugins/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="/js/sb-admin-2.js"></script>
+
+	  <!-- jQuery Version 1.11.0 -->
+    <script src="/js/jquery-1.11.0.js"></script>
+    <!--<script src="jquery.js"></script>-->
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="/js/plugins/metisMenu/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="/js/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="/js/sb-admin-2.js"></script>
+
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script>
+    $(document).ready(function() {
+        $('#dataTables-example').dataTable();
+    });
+    </script>
+
+<?php
+/*            Local              */
+/*
+<!-- jQuery Version 1.11.0 -->
     <script src="/XVote/js/jquery-1.11.0.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
@@ -8,11 +47,7 @@
     <script src="/XVote/js/plugins/metisMenu/metisMenu.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="/XVote/js/sb-admin-2.js"></script>
-	
-	
-	<!--DAta-table-->
-	
+    <script src="/XVote/js/sb-admin-2.js"></script>	
 	
 	  <!-- jQuery Version 1.11.0 -->
     <script src="/XVote/js/jquery-1.11.0.js"></script>
@@ -37,3 +72,5 @@
         $('#dataTables-example').dataTable();
     });
     </script>
+*/
+?>
---
+++ to do.md
@@ -0,0 +1,15 @@
+- [ ] Change fontawesome to latest version and change all icons
+- [ ] Add cesa logo on all login pages
+- [ ] Add modal for login errors
+- [ ] Modify "Submit Ballot" button on vote.php , vote_result.php
+- [ ] Integrate XApp/XLogin 
+- [ ] Deploy
+- [ ] Revoke token
+- [ ] Manage errors on SQL Operations
+- [ ] Propagate using of Env
+- [ ] Check require vs include using
+- [ ] Progress bar
+
+# For deployment
+
+- Change absolute path in scripts/script.php
