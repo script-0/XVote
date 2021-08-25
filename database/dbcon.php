043d9c8bd7e7ba20a74a0acee55754b62d766790
isaac <ndemabekolleemmanuelisaac@gmail.com>
2021-08-25 03:55:08 +0100

    Commit before deploying on Heroku

--- database/dbcon.php
+++ database/dbcon.php
@@ -1,9 +1,9 @@
 <?php
 	//To Deploy
-	//$conn = new mysqli('db4free.net', 'cesa_user', 'cesa_password', 'cesa_vote');
+	$conn = new mysqli('db4free.net', 'cesa_user', 'cesa_password', 'cesa_vote');
 
 	//Local
-	$conn = new mysqli('127.0.0.1', 'root', 'password', 'cesa_vote');
+	//$conn = new mysqli('127.0.0.1', 'root', 'password', 'cesa_vote');
 	
 	if(!$conn){
 		die("Error: Failed to connect to database");
--- database/init_db.sql
+++ database/init_db.sql
@@ -16,8 +16,8 @@ SET time_zone = "+00:00";
 --
 -- Drop Database if exists and reCreate Database 
 --
-DROP DATABASE IF EXISTS `cesa_vote`;
-CREATE DATABASE IF NOT EXISTS `cesa_vote`;
+-- DROP DATABASE IF EXISTS `cesa_vote`;
+-- CREATE DATABASE IF NOT EXISTS `cesa_vote`;
 
 --
 -- Select database
