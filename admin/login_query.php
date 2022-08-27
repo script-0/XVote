1148e9bcc1f64c45c5b170c71651b64d4646cdbb
script-0 <ndemabekolleemmanuelisaac@gmail.com>
2022-08-28 00:55:57 +0100

    Hashing everything

--- admin/login_query.php
+++ admin/login_query.php
@@ -7,20 +7,21 @@
 		$password=$_POST['password'];
 	
 		
-		$query = $conn->query("SELECT * FROM user WHERE username = 	'$username' AND password = '$password'") or die(mysql_error());
+		$query = $conn->query("SELECT * FROM user WHERE username = 	'$username'") or die(mysqli_error($conn));
 		$rows = $query->num_rows;
 		$fetch = $query->fetch_array();
-																		
-			if ($rows == 0) 
-					{
-						echo " <br><center><font color= 'red' size='3'>Please fill up the fields correctly</center></font>";
-					} 
-				else if ($rows > 0)
-					{
-					session_start();
-					$_SESSION['id'] = $fetch['user_id'];
-					header("location:candidate.php");
-				
+		
+		$isPasswordCorrect = password_verify($password,$fetch['password']);								
+			if ($rows == 0){
+				echo " <br><center><font color= 'red' size='3'>Please fill up the fields correctly</font></center>";
+			}else if ($rows > 0){
+				if(!$isPasswordCorrect){	
+					echo " <br><center><font color= 'red' size='3'>Your password or username seems wrong.</font></center>";
+					die();
+				}
+				session_start();
+				$_SESSION['id'] = $fetch['user_id'];
+				header("location:candidate.php");
 			}	
 	
 	}
--- login_query.php
+++ login_query.php
@@ -4,10 +4,20 @@
 	if(isset($_POST['login'])){
 		$idno=$_POST['idno'];
 		$password=$_POST['password'];
-		$result = $conn->query("SELECT voters_id, status, account FROM voters WHERE id_number = '$idno' && password = '$password'") or die(mysqli_errno());
+		$result = $conn->query("SELECT voters_id, status, account, password FROM voters WHERE id_number = '$idno' ") or die('Unknown User.');
 		$numberOfRows = $result->num_rows;
 		if($numberOfRows > 0){
 			$row = $result->fetch_array();
+			$isPasswordCorrect = password_verify($password,$row['password']);
+			if(!$isPasswordCorrect){
+				echo " <center>
+						<br>
+						<font color= 'red' size='3'>Bad Matricule / password </font>
+						<br>
+						Results Available <a href='results.php?token=CESAGI2022'> here </a>
+				   </center>";
+				die();
+			}
 			if($row['status'] =='Voted'){		
 				echo " <br>
 						<center>
